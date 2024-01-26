a92630b1f47317d60638f7ce3f0d9d309da09cf1
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2024-01-26 10:58:40 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test</title>
+</head>
+<body>
+    
+<?php
+echo "<h1>hello wold </h1>";
+//this printing hello world 
+
+/* 
+multiple 
+line comments 
+
+*/
+
+
+?>
+
+
+</body>
+</html>
+
