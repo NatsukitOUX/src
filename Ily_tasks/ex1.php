7b842e1361a90494277eac4d72d1357b98e5339a
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2024-01-28 20:18:18 +0200

    .

--- Ily_tasks/ex1.php
+++ Ily_tasks/ex1.php
@@ -32,39 +32,42 @@ echo "<h4>$title</h4>" ;
 </h3>
 
 <?php
-
 $g1 = 5;
 $g2 = 4;
 $g3 = 5;
 ?>
 
 <table>
-    <tr> S.n</tr><tr> Name</tr> <tr> Grade</tr> 
-     <tr>
+    <tr>
+        <th>S.n</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+
+    <tr>
         <td>1</td>
         <td>John</td>
-        <td> <?php  echo $g1   ?> </td>
-     </tr>
+        <td><?php echo $g1; ?></td>
+    </tr>
 
-     <tr>
-     <td>2</td>
+    <tr>
+        <td>2</td>
         <td>Alice</td>
-        <td> <?php  echo $g2   ?> </td>
-     </tr>
+        <td><?php echo $g2; ?></td>
+    </tr>
 
-     <tr>
-     <td>3</td>
+    <tr>
+        <td>3</td>
         <td>Bob</td>
-        <td> <?php  echo $g3   ?> </td>
-     </tr>
+        <td><?php echo $g3; ?></td>
+    </tr>
 </table>
-
 
 <h3> Take a screenshot that confirms your 
 development environment setup and include 
 it as an image in the "ex1.php" file.</h3>
 
-<img src="images/24php.png" alt="docker image" >\
+<img src="images/24php.png" alt="docker image" >
 
 </body>
 </html>
