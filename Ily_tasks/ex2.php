34b181237e07cd717d31b46af011559eebe2b206
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2024-01-29 11:13:03 +0200

    ex2 Done

---
+++ Ily_tasks/ex2.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+<div class="col -md-8">  
+          <h1>HTML Assignment</h1>
+    <p>Bootstrap utilizes Sass for a modular and customizable architecture. Import only the components you need, enable global options like gradients and shadows, and write your own CSS with our variables, maps, functions, and mixinsImport only the components you need, enable global options like gradients and shadows, and write your own CSS with our variables, maps, functions, and mixins</p>
+    
+    
+</div>
+
+
+<? include 'footer.php';?>
